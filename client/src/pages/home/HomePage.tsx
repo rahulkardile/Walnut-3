083df18a52837d66f2ba1adca4a52544aa4df544
Rahul kardile <rahulkardile321@gmail.com>
2024-06-04 09:38:01 +0530

    major ui update

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
 import BannerHome from "../../components/BannerHome/BannerHome";
 import Navbar from "../../components/nav/Navbar";
 import SideNav from "../../components/nav/SideNav";
 import VisionAndMission from "../../components/vision/VisionAndMission";
 
-
-
 const HomePage = () => {
-
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
     <div className="overscroll-none flex flex-col gap-3">
       <BannerHome />
-      <SideNav />
-      <Navbar />
+      <SideNav
+        isNavOpen={isNavOpen}
+        toggleNav={() => {
+          setIsNavOpen(!isNavOpen);
+        }}
+      />
+      {/* <Navbar /> */}
       <VisionAndMission />
     </div>
   );
